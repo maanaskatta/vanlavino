31135add474b07e8ae3f5468af80c3020091265e
maanaskatta <maanaskatta6@gmail.com>
2021-11-16 09:52:06 -0600

    Completed manager dashboard

--- src/pages/Manager/index.js
+++ src/pages/Manager/index.js
@@ -1,9 +1,195 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  AiOutlineCar,
+  AiOutlineHistory,
+  AiOutlineReconciliation,
+} from "react-icons/ai";
+import { BiCategory, BiNotepad } from "react-icons/bi";
+import { BsCardList, BsReceipt } from "react-icons/bs";
+import { FaCoins, FaTools, FaUsers } from "react-icons/fa";
+import { GiChefToque } from "react-icons/gi";
+import { HiOutlineSpeakerphone } from "react-icons/hi";
+import { IoMdPhotos } from "react-icons/io";
+import {
+  MdOutlineFeaturedPlayList,
+  MdOutlineRestaurantMenu,
+} from "react-icons/md";
+import { Link, Redirect, Route, Switch, useHistory } from "react-router-dom";
+import Logo from "../../images/Logo.png";
+
+const managerTiles = [
+  {
+    label: "Chefs",
+    path: "/chefs",
+    icon: () => <GiChefToque />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Cuisines",
+    path: "/cuisines",
+    icon: () => <BiCategory />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Menu",
+    path: "/menu",
+    icon: () => <MdOutlineRestaurantMenu />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Orders",
+    path: "/orders",
+    icon: () => <BiNotepad />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Events",
+    path: "/events",
+    icon: () => <AiOutlineHistory />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Managers",
+    path: "/managers",
+    icon: () => <FaTools />,
+    render: (label) => <p>{label}</p>,
+  },
+
+  {
+    label: "Department",
+    path: "/department",
+    icon: () => <BiCategory />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Table Reservations",
+    path: "/reservations",
+    icon: () => <FaCoins />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Coupons",
+    path: "/coupons",
+    icon: () => <AiOutlineReconciliation />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Customers",
+    path: "/customers",
+    icon: () => <MdOutlineFeaturedPlayList />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Daily Report",
+    path: "/dailyReport",
+    icon: () => <BsCardList />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Salaries",
+    path: "/salaries",
+    icon: () => <IoMdPhotos />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Reviews",
+    path: "/reviews",
+    icon: () => <HiOutlineSpeakerphone />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Staff",
+    path: "/staff",
+    icon: () => <HiOutlineSpeakerphone />,
+    render: (label) => <p>{label}</p>,
+  },
+  {
+    label: "Stewards",
+    path: "/stewards",
+    icon: () => <HiOutlineSpeakerphone />,
+    render: (label) => <p>{label}</p>,
+  },
+];
+
+const HeaderNav = () => {
+  const history = useHistory();
+  const [currentTileIndex, setCurrentTileIndex] = useState(0);
+
+  return (
+    <div className="flex flex-col w-full gap-2 py-4 mx-4">
+      <div className="flex  gap-3 justify-between items-center">
+        <div>
+          <img src={Logo} alt="" width={150} />
+        </div>
+        <p className="text-4xl font-bold text-white italic tracking-wide">
+          Van Lavino
+        </p>
+        <div>
+          <button
+            onClick={() => {
+              history.push("/login");
+              localStorage.setItem("role", null);
+            }}
+            className="bg-red-500 text-white px-3 py-2 rounded"
+          >
+            Sign Out
+          </button>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-7 gap-2">
+        {managerTiles.map((tile, index) => {
+          return (
+            <div>
+              <Link
+                to={`/manager` + tile.path}
+                className={`${
+                  index === currentTileIndex
+                    ? "bg-white text-blue-600"
+                    : "text-white"
+                } flex justify-center items-center border py-4 font-semibold`}
+                onClick={() => {
+                  setCurrentTileIndex(index);
+                }}
+              >
+                <div className="flex items-center gap-3">
+                  <div
+                    className={`text-2xl ${
+                      index === currentTileIndex
+                        ? "text-blue-600"
+                        : "text-white"
+                    }`}
+                  >
+                    {tile.icon()}
+                  </div>
+                  {tile.label}
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default function Manager() {
   return (
-    <div>
-      <p>Manager</p>
+    <div className="flex w-full flex-col ">
+      <div className="flex w-full bg-blue-600">
+        <HeaderNav />
+      </div>
+      <div className="w-full flex flex-col p-4">
+        <Switch>
+          {managerTiles.map((tile) => (
+            <Route
+              path={`/manager` + tile.path}
+              component={() => tile.render(tile.label)}
+            />
+          ))}
+        </Switch>
+        <Redirect to={`/manager` + managerTiles[0].path} />
+      </div>
     </div>
   );
 }
